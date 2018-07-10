29431de9f8a21e57042b1ac839228446c630edb6
hexing521 <1039380441@qq.com>
2018-07-10 16:15:03 +0800

    添加kafka消费案例二

---
+++ src/main/java/kafka/ConsumerRunnable.java
@@ -0,0 +1,88 @@
+package kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+
+/**
+ * 模拟每个线程维护一个consumer的消费操作
+ * 消费和处理数据在同一个线程中完成
+ *
+ */
+public class ConsumerRunnable implements Runnable {
+
+
+    // 每个线程维护私有的KafkaConsumer实例
+    private final KafkaConsumer<String, String> consumer;
+
+    public ConsumerRunnable(String brokerList, String groupId, String topic) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", brokerList);
+        props.put("group.id", groupId);
+        props.put("enable.auto.commit", "true");        //本例使用自动提交位移
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        this.consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList(topic));   // 本例使用分区副本自动分配策略
+    }
+
+
+    public void run() {
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(200);   // 本例使用200ms作为获取超时时间
+            for (ConsumerRecord<String, String> record : records) {
+                // 这里面写处理消息的逻辑，本例中只是简单地打印消息
+                System.out.println(Thread.currentThread().getName() + " consumed " + record.partition() +
+                        "th message with offset: " + record.offset());
+            }
+        }
+    }
+
+}
+
+/**
+ * 消费组
+ */
+class ConsumerGroup {
+
+    private List<ConsumerRunnable> consumers;
+
+    public ConsumerGroup(int consumerNum, String groupId, String topic, String brokerList) {
+        consumers = new ArrayList<ConsumerRunnable>(consumerNum);
+        for (int i = 0; i < consumerNum; ++i) {
+            ConsumerRunnable consumerThread = new ConsumerRunnable(brokerList, groupId, topic);
+            consumers.add(consumerThread);
+        }
+    }
+
+    public void execute() {
+        for (ConsumerRunnable task : consumers) {
+            new Thread(task).start();
+        }
+    }
+}
+
+
+/**
+ * 测试类
+ */
+class ConsumerMain {
+
+    public static void main(String[] args) {
+        String brokerList = "localhost:9092";
+        String groupId = "testGroup1";
+        String topic = "test-topic";
+        int consumerNum = 3;
+
+        ConsumerGroup consumerGroup = new ConsumerGroup(consumerNum, groupId, topic, brokerList);
+        consumerGroup.execute();
+    }
+}
