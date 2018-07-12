08f02f3e0e57a2c264393379b8c67822ff166f8b
hexing521 <1039380441@qq.com>
2018-07-12 16:13:44 +0800

    scala断言使用测试

---
+++ src/main/scala/Scalas/PredefDemo.scala
@@ -0,0 +1,16 @@
+package Scalas
+
+/**
+  * scala 断言使用测试
+  */
+object PredefDemo {
+
+  def main(args: Array[String]): Unit = {
+
+    require(1==1)
+    assert(2==1,"excption compire unEquire")
+    print("-----------------")
+
+  }
+
+}
