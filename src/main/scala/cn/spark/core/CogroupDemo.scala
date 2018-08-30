b9139dcbf7e5b97bc2d1c5e4114e88ae4094edad
阿星 <axing@axingdeMacBook-Pro.local>
2018-08-30 14:23:39 +0800

    commit code

--- src/main/scala/cn/spark/core/CogroupDemo.scala
+++ src/main/scala/cn/spark/core/CogroupDemo.scala
@@ -16,6 +16,16 @@ object CogroupDemo {
     val data3 = sc.parallelize(List((1, "com"), (2, "com"), (3, "good")))
     val result = data1.cogroup(data2, data3)
     result.collect.foreach(println)
+    //todo  输出结果
+    //(2,(CompactBuffer(bbs),CompactBuffer(iteblog),CompactBuffer(com)))
+    //(1,(CompactBuffer(www),CompactBuffer(iteblog),CompactBuffer(com)))
+    //(3,(CompactBuffer(),CompactBuffer(very),CompactBuffer(good)))
+
+    //取出所有的key value
+    result.foreach(item => {
+      println("key------" + item._1)
+      println("value----" + item._2._1.mkString(""))
+    })
 
 
   }
