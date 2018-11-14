e6eb9a50f3941f92cef3ade82b845593bd6b2633
xingxingt <1039380441@qq.com>
2018-11-14 17:22:47 +0800

    update code

--- src/main/java/proxys/LogInvocationHandler.java
+++ src/main/java/proxys/LogInvocationHandler.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 动态代理实现案例
+ */
 // 接口
 interface Hello {
     String sayHello(String str);
@@ -20,34 +23,39 @@ class HelloImp implements Hello {
     }
 }
 
+//代理类
 public class LogInvocationHandler implements InvocationHandler {
     private static Logger logger = Logger.getLogger(LogInvocationHandler.class);
-
+    //目标类，在LogInvocationHandler实例化的时候传入进来
     private Hello hello;
 
     public LogInvocationHandler(Hello hello) {
         this.hello = hello;
     }
 
-
+    //关键方法
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if ("sayHello".equals(method.getName())) {
             logger.info("You said: " + Arrays.toString(args));
         }
-        return method.invoke(hello, args);
+
+        logger.info("------实现前置通知------" );
+        //代理对象调用转发给目标对象
+        Object obj=method.invoke(hello, args);
+        logger.info("------实现后置通知------" );
+
+        return obj;
     }
 
 }
 
-//class LogInvocationHandlerTest {
-//    public static void main(String[] args) {
-//
-//        // 2. 然后在需要使用Hello的时候，通过JDK动态代理获取Hello的代理对象。
-//        Hello hello = (Hello) Proxy.newProxyInstance(
-//                getClass().getClassLoader(), // 1. 类加载器
-//                new Class<?>[]{Hello.class}, // 2. 代理需要实现的接口，可以有多个
-//                new LogInvocationHandler(new HelloImp()));// 3. 方法调用的实际处理者
-//        System.out.println(hello.sayHello("I love you!"));
-//
-//    }
-//}
+class LogInvocationHandlerTest {
+    public static void main(String[] args) {
+        // 然后在需要使用Hello的时候，通过JDK动态代理获取Hello的代理对象。
+        Hello hello = (Hello) Proxy.newProxyInstance(LogInvocationHandler.class.getClassLoader(), // 1. 类加载器
+                new Class<?>[]{Hello.class}, // 2. 代理需要实现的接口，可以有多个
+                new LogInvocationHandler(new HelloImp()));// 3. 方法调用的实际处理者
+        System.out.println(hello.sayHello("I love you!"));
+
+    }
+}
