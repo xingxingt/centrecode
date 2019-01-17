22699b1eabb22f8494e1ae81a56f9db9aa9d12d6
xingxingt <1039380441@qq.com>
2019-01-17 19:23:25 +0800

    commit merger sort code

--- src/main/scala/cn/spark/core/RDDApiTest.scala
+++ src/main/scala/cn/spark/core/RDDApiTest.scala
@@ -5,7 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object RDDApiTest {
 
   def main(args: Array[String]): Unit = {
-    var sc = getSparkContext()
+    val sc = getSparkContext()
     //    joinTransformation(sc)
     //    leftOuterJoinTransformation(sc)
     //    rightOuterJoinTransformation(sc)
@@ -21,7 +21,7 @@ object RDDApiTest {
   }
 
   def getSparkContext() = {
-    var sconf = new SparkConf()
+    val sconf = new SparkConf()
     sconf.setMaster("local")
     sconf.setAppName("RDDTest")
     new SparkContext(sconf)
@@ -29,63 +29,63 @@ object RDDApiTest {
 
 
   def joinTransformation(sc: SparkContext) = {
-    var classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"), Tuple2("4", "hive"), Tuple2("5", "java"))
-    var scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90))
-    var classRdd = sc.parallelize(classArray)
-    var scoreRdd = sc.parallelize(scoreArray)
-    var joinRdd = classRdd.join(scoreRdd)
+    val classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"), Tuple2("4", "hive"), Tuple2("5", "java"))
+    val scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90))
+    val classRdd = sc.parallelize(classArray)
+    val scoreRdd = sc.parallelize(scoreArray)
+    val joinRdd = classRdd.join(scoreRdd)
     joinRdd.collect().foreach(item => println(s"join:${item._1}----${item._2}"))
   }
 
   def leftOuterJoinTransformation(sc: SparkContext) = {
-    var classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"))
-    var scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90))
-    var classRdd = sc.parallelize(classArray)
-    var scoreRdd = sc.parallelize(scoreArray)
-    var joinRdd = classRdd.leftOuterJoin(scoreRdd)
+    val classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"))
+    val scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90))
+    val classRdd = sc.parallelize(classArray)
+    val scoreRdd = sc.parallelize(scoreArray)
+    val joinRdd = classRdd.leftOuterJoin(scoreRdd)
     joinRdd.collect().foreach(item => println(s"leftOuterJoin:${item._1}----${item._2}"))
   }
 
   def rightOuterJoinTransformation(sc: SparkContext) = {
-    var classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"), Tuple2("4", "hive"))
-    var scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90), Tuple2("5", 90))
-    var classRdd = sc.parallelize(classArray)
-    var scoreRdd = sc.parallelize(scoreArray)
-    var joinRdd = classRdd.rightOuterJoin(scoreRdd)
+    val classArray = Array(Tuple2("1", "spark"), Tuple2("2", "scala"), Tuple2("3", "hadoop"), Tuple2("4", "hive"))
+    val scoreArray = Array(Tuple2("1", 99), Tuple2("2", 90), Tuple2("3", 89), Tuple2("4", 90), Tuple2("5", 90))
+    val classRdd = sc.parallelize(classArray)
+    val scoreRdd = sc.parallelize(scoreArray)
+    val joinRdd = classRdd.rightOuterJoin(scoreRdd)
     joinRdd.collect().foreach(item => println(s"rightOuterJoin:${item._1}----${item._2}"))
   }
 
   /*传入一个RDD*/
   def cogroup1Transformation(sc: SparkContext) = {
-    var rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
-    var rdd2 = sc.makeRDD(Array(("A", "a"), ("A", "5"), ("C", "c"), ("D", "d")), 2)
-    var rdd3 = rdd1.cogroup(rdd2)
+    val rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
+    val rdd2 = sc.makeRDD(Array(("A", "a"), ("A", "5"), ("C", "c"), ("D", "d")), 2)
+    val rdd3 = rdd1.cogroup(rdd2)
     rdd3.collect.foreach(item => println(s"cogroup1:${item._1}----${item._2}"))
   }
 
   /*传入两个RDD*/
   def cogroup2Transformation(sc: SparkContext) = {
-    var rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
-    var rdd2 = sc.makeRDD(Array(("A", "a"), ("C", "c"), ("D", "d")), 2)
-    var rdd3 = sc.makeRDD(Array(("A", "A"), ("E", "E"), ("F", "h")), 2)
-    var rdd4 = rdd1.cogroup(rdd2, rdd3)
+    val rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
+    val rdd2 = sc.makeRDD(Array(("A", "a"), ("C", "c"), ("D", "d")), 2)
+    val rdd3 = sc.makeRDD(Array(("A", "A"), ("E", "E"), ("F", "h")), 2)
+    val rdd4 = rdd1.cogroup(rdd2, rdd3)
     rdd4.collect.foreach(item => println(s"cogroup2:${item._1}----${item._2}"))
   }
 
   /*rdd重分区*/
-  def coalesceTransformation(sc: SparkContext) = {
-    var rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
-    var rdd2 = rdd1.coalesce(1, false)
-    var rdd3 = rdd1.coalesce(4, true)
+  def coalesceTransformation(sc: SparkContext): Unit = {
+    val rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
+    val rdd2 = rdd1.coalesce(1, false)
+    val rdd3 = rdd1.coalesce(4, true)
     println(rdd3.partitions.size)
   }
 
   /**/
   def mapPartitionsWithIndexTransformation(sc: SparkContext) = {
-    var rdd1 = sc.makeRDD(1 to 5, 2)
-    var rdd2 = rdd1.mapPartitionsWithIndex {
+    val rdd1 = sc.makeRDD(1 to 5, 2)
+    val rdd2 = rdd1.mapPartitionsWithIndex {
       (x, iter) => {
-        var result = List[String]()
+        val result = List[String]()
         var i = 0
         while (iter.hasNext) {
           i += iter.next()
@@ -99,7 +99,7 @@ object RDDApiTest {
 
   //该函数用于将RDD[K,V]中每个K对应的V值，合并到一个集合Iterable[V]中，
   def groupByKeyDemo(sc: SparkContext) = {
-    var rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
+    val rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
     //参数numPartitions用于指定分区数；
     //参数partitioner用于指定分区函数
     val rdd2 = rdd1.groupByKey(1)
@@ -108,10 +108,10 @@ object RDDApiTest {
 
   //该函数用于将RDD[K,V]中每个K对应的V值根据映射函数来运算。
   def reduceByKeyDemo(sc: SparkContext): Unit = {
-    var rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
+    val rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
     //参数numPartitions用于指定分区数；
     //参数partitioner用于指定分区函数
-    var rdd2 = rdd1.reduceByKey(new org.apache.spark.HashPartitioner(2), (x, y) => x + y)
+    val rdd2 = rdd1.reduceByKey(new org.apache.spark.HashPartitioner(2), (x, y) => x + y)
     rdd2.collect().foreach(item => println(item))
 
   }
