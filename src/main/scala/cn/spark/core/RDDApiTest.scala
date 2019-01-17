998334e356615f58985641b61212462c4008f922
xingxingt <1039380441@qq.com>
2019-01-17 19:21:27 +0800

    commit merger sort code

--- src/main/scala/cn/spark/core/RDDApiTest.scala
+++ src/main/scala/cn/spark/core/RDDApiTest.scala
@@ -2,17 +2,20 @@ package cn.spark.core
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object RDDTest {
+object RDDApiTest {
 
   def main(args: Array[String]): Unit = {
     var sc = getSparkContext()
     //    joinTransformation(sc)
     //    leftOuterJoinTransformation(sc)
     //    rightOuterJoinTransformation(sc)
-    cogroup1Transformation(sc)
-    cogroup2Transformation(sc)
+    //    cogroup1Transformation(sc)
+    //    cogroup2Transformation(sc)
     //    coalesceTransformation(sc)
     //    mapPartitionsWithIndexTransformation(sc)
+    groupByKeyDemo(sc)
+    reduceByKeyDemo(sc)
+    reduceByKeyLocallyDemo(sc)
 
 
   }
@@ -55,7 +58,7 @@ object RDDTest {
   /*传入一个RDD*/
   def cogroup1Transformation(sc: SparkContext) = {
     var rdd1 = sc.makeRDD(Array(("A", "1"), ("B", "2"), ("C", "3")), 2)
-    var rdd2 = sc.makeRDD(Array(("A", "a"),("A", "5"), ("C", "c"), ("D", "d")), 2)
+    var rdd2 = sc.makeRDD(Array(("A", "a"), ("A", "5"), ("C", "c"), ("D", "d")), 2)
     var rdd3 = rdd1.cogroup(rdd2)
     rdd3.collect.foreach(item => println(s"cogroup1:${item._1}----${item._2}"))
   }
@@ -91,6 +94,33 @@ object RDDTest {
       }
     }
     rdd2.collect().foreach(item => println(item))
+
+  }
+
+  //该函数用于将RDD[K,V]中每个K对应的V值，合并到一个集合Iterable[V]中，
+  def groupByKeyDemo(sc: SparkContext) = {
+    var rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
+    //参数numPartitions用于指定分区数；
+    //参数partitioner用于指定分区函数
+    val rdd2 = rdd1.groupByKey(1)
+    rdd2.collect().foreach(item => println(item))
+  }
+
+  //该函数用于将RDD[K,V]中每个K对应的V值根据映射函数来运算。
+  def reduceByKeyDemo(sc: SparkContext): Unit = {
+    var rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
+    //参数numPartitions用于指定分区数；
+    //参数partitioner用于指定分区函数
+    var rdd2 = rdd1.reduceByKey(new org.apache.spark.HashPartitioner(2), (x, y) => x + y)
+    rdd2.collect().foreach(item => println(item))
+
+  }
+
+  //该函数将RDD[K,V]中每个K对应的V值根据映射函数来运算，运算结果映射到一个Map[K,V]中，而不是RDD[K,V]。
+  def reduceByKeyLocallyDemo(sc: SparkContext): Unit = {
+    val rdd1 = sc.makeRDD(Array(("A", 0), ("A", 2), ("B", 1), ("B", 2), ("C", 1)))
+    val rdd2 = rdd1.reduceByKeyLocally((x, y) => x + y)
+    rdd2.foreach(item => println(item))
 
   }
 
