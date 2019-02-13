fe4bf963dd4a98ff3f9a47f86861af267cb0836a
xingxingt <1039380441@qq.com>
2019-02-13 21:52:01 +0800

    commit code

---
+++ src/main/java/dataStructure/SkipListDemo.java
@@ -0,0 +1,126 @@
+package dataStructure;
+
+import java.util.Random;
+
+/**
+ * 跳表是一种空间换时间的数据结构
+ * 跳表的一种实现方法。
+ * 跳表中存储的是正整数，并且存储的是不重复的。
+ * <p>
+ * 时间复杂度为O(logn)
+ * 用于建立索引的空间复杂度为O(n) 但是往往忽略掉建立索引所消耗的空间，因为和存储的对象相比索引要小的多的多
+ * 插入,删除，查找数据的时间复杂度为O(logn)
+ */
+public class SkipListDemo {
+
+    private static final int MAX_LEVEL = 16;
+
+    private int levelCount = 1;
+
+    private Node head = new Node();  // 带头链表
+
+    private Random r = new Random();
+
+    public Node find(int value) {
+        Node p = head;
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value) {
+            return p.forwards[0];
+        } else {
+            return null;
+        }
+    }
+
+    public void insert(int value) {
+        int level = randomLevel();
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.maxLevel = level;
+        Node update[] = new Node[level];
+        for (int i = 0; i < level; ++i) {
+            update[i] = head;
+        }
+
+        // record every level largest value which smaller than insert value in update[]
+        Node p = head;
+        for (int i = level - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;// use update save node in search path
+        }
+
+        // in search path node next node become new node forwords(next)
+        for (int i = 0; i < level; ++i) {
+            newNode.forwards[i] = update[i].forwards[i];
+            update[i].forwards[i] = newNode;
+        }
+
+        // update node hight
+        if (levelCount < level) levelCount = level;
+    }
+
+    public void delete(int value) {
+        Node[] update = new Node[levelCount];
+        Node p = head;
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value) {
+            for (int i = levelCount - 1; i >= 0; --i) {
+                if (update[i].forwards[i] != null && update[i].forwards[i].data == value) {
+                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
+                }
+            }
+        }
+    }
+
+    // 随机 level 次，如果是奇数层数 +1，防止伪随机
+    private int randomLevel() {
+        int level = 1;
+        for (int i = 1; i < MAX_LEVEL; ++i) {
+            if (r.nextInt() % 2 == 1) {
+                level++;
+            }
+        }
+
+        return level;
+    }
+
+    public void printAll() {
+        Node p = head;
+        while (p.forwards[0] != null) {
+            System.out.print(p.forwards[0] + " ");
+            p = p.forwards[0];
+        }
+        System.out.println();
+    }
+
+    public class Node {
+        private int data = -1;
+        private Node forwards[] = new Node[MAX_LEVEL];
+        private int maxLevel = 0;
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append("; levels: ");
+            builder.append(maxLevel);
+            builder.append(" }");
+
+            return builder.toString();
+        }
+    }
+
+}
